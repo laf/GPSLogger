8267abc1101eeec74bfceb0c8ed311f37c06db25
Chris Joyce <chris@joyce.id.au>
2013-08-11 14:12:58 +1000

    Added Make system
    Using 'make' and make 'cleandb' the setup of GPSlogger will become much easer
    Added 'make help' to assist with use of make
    Added DB name to config (only fir use in gpsmakedb.py at this time)

---
+++ gpsmakedb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Script created by Chris Joyce <chris@joyce.id.au>
+
+import sqlite3 as lite
+import sys
+from config import config
+
+extras = (
+    (1, 'dbversion', '2', 'INT'),
+    (2, 'hasrunonce', 'true', 'BOOL')
+)
+
+
+con = lite.connect(config['DB_NAME'])
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS extra")
+	cur.execute("DROP TABLE IF EXISTS gpslog")
+    cur.execute("CREATE TABLE extra(id INT, name varchar(32), value varchar(100), casttype varchar(32))")
+	cur.execute("CREATE TABLE gpslog (id INTEGER PRIMARY KEY,datetime varchar(30),lon varchar(100),lat varchar(100),alt varchar(100),speed varchar(100),uploaded varchar(1),session_id INT)"
+    cur.executemany("INSERT INTO extra VALUES(?, ?, ?)", extras)
